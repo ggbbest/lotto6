bcbf1dcac638a08dfe7acdf054d563b9fbec5fcb
ggbbest <www.ggb.best@gmail.com>
2022-03-13 22:42:18 +0900

    fav

--- sql/lotto.sql
+++ sql/lotto.sql
@@ -24,17 +24,40 @@ CREATE TABLE `lotto` (
 	`numb5` TINYINT(2) NOT NULL,
 	`numb6` TINYINT(2) NOT NULL,
 	`addr` VARCHAR(70) NOT NULL COLLATE 'utf8_unicode_ci',
-	`sendTr` VARCHAR(300) NULL COLLATE 'utf8_unicode_ci',
+	`sendTr` VARCHAR(100) NULL COLLATE 'utf8_unicode_ci',
 	`win_rank` INT(1) NOT NULL DEFAULT '0',
 	`regdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`idx`) USING BTREE
 )
 COLLATE='utf8_unicode_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=11
+AUTO_INCREMENT=1
+;
+--////////////////////////////////////////////////////////////////////////////////////////////////
+CREATE TABLE `lotto_num` (
+	`idx` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`yyyy` VARCHAR(4) NOT NULL COLLATE 'utf8_unicode_ci',
+	`wk` VARCHAR(2) NOT NULL COLLATE 'utf8_unicode_ci',
+	`yyyymmdd` VARCHAR(8) NOT NULL COLLATE 'utf8_unicode_ci',
+	`sum_chips` INT(4) NOT NULL,
+	`numb_tot` VARCHAR(20) NULL COLLATE 'utf8_unicode_ci',
+	`numb1` TINYINT(2) NOT NULL,
+	`numb2` TINYINT(2) NOT NULL,
+	`numb3` TINYINT(2) NOT NULL,
+	`numb4` TINYINT(2) NOT NULL,
+	`numb5` TINYINT(2) NOT NULL,
+	`numb6` TINYINT(2) NOT NULL,
+	`regdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`idx`) USING BTREE
+)
+COLLATE='utf8_unicode_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
 ;
 
-
+INSERT INTO lotto_num (`yyyy`,`wk`,`yyyymmdd`) SELECT '2202','12','22020326' FROM DUAL;
+COMMIT;
+--////////////////////////////////////////////////////////////////////////////////////////////////
 SELECT YEAR(DATETIME) AS YEAR
 SELECT DATE_FORMAT(NOW(), '%Y%m%d') AS today_date FROM DUAL;
 SELECT concat( DATE_FORMAT(NOW(), '%Y') ,'_' ,WEEK(NOW()) ); 
