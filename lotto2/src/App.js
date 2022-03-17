375f45a8389cf083a9f070089695eb88f62721d3
ggbbest <www.ggb.best@gmail.com>
2022-03-17 15:04:57 +0900

    code clean

--- lotto2/src/App.js
+++ lotto2/src/App.js
@@ -1,32 +1,17 @@
 import "./App.css";
 import React, { useState } from "react";
-
 import Header from "./Header";
 import Results from "./Results";
 import Coupon from "./Coupon";
 import ButtonStart from "./ButtonStart";
 import Display from "./Display";
 import ButtonReset from "./ButtonReset";
-
 import axios from 'axios'; 
-
 ///////////////////// metamask s ////////////////
 import {useWeb3React} from '@web3-react/core';
 import {injected} from './lib/connectors';
-// import detectEthereumProvider from '@metamask/detect-provider'
 ///////////////////// metamask e ////////////////
-// import {ethTX} from 'ethereumjs-tx'; //yarn remove ethereumjs-tx
-//yarn add web3
 import { useAlert } from 'react-alert'
-// import { json } from "express/lib/response";
-//yarn remove reactjs-popup
-// import Popup from 'reactjs-popup';
-// import 'reactjs-popup/dist/index.css';
-// export default () => (
-//   <Popup trigger={<button> Trigger</button>} position="right center">
-//     <div>Popup content here !!</div>
-//   </Popup>
-// );
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -40,7 +25,6 @@ const App = () => {
     21,22,23,24,25,26,27,28,29,30,
     31,32,33,34,35,36,37,38,39,40,
     41,42,43,44,45
-    // ,46,47,48,49,
   ];
   const awards = { three: 5, four: 50, five: 5000, six: 3000000 };
 
@@ -53,124 +37,10 @@ const App = () => {
   const selectListChip = ["1", "2", "3", "4", "5", "10", "20", "30", "40", "50", "100", "500", "1000"];
   const [SelectedChip, setSelectedChip] = useState("");
   const handleSelectChip = (e) => { setSelectedChip(e.target.value); };
-  // setSelectedChip("1");
    //////////////////////////////////////////////////////
-  
-  const addPlayerNumbers = (number, event) => {
-    if (playerNumbers.length < 6 && !playerNumbers.includes(number)) {
-      const numbers = [...playerNumbers];
-      numbers.push(number);
-      setPlayerNumbers(numbers);
-      event.target.classList.toggle("selected");
-    }
-    if (playerNumbers.includes(number)) {
-      let numbers = [...playerNumbers];
-      numbers = numbers.filter((num) => num !== number);
-      setPlayerNumbers(numbers);
-      event.target.classList.toggle("selected");
-    }
-  };
-
-  const checkWin = (playerNumbers, drawedNumbers) => {
-    const winNumbers = [];
-
-    playerNumbers.forEach(function (number) {
-      for (let i = 0; i < 6; i++) {
-        if (number === drawedNumbers[i]) {
-          winNumbers.push(number);
-        }
-      }
-    });
-
-    const hits = winNumbers.length;
-    setHits(hits);
-
-    if (hits === 3) {
-      setMoney((prev) => prev + awards.three);
-    } else if (hits === 4) {
-      setMoney((prev) => prev + awards.four);
-    } else if (hits === 5) {
-      setMoney((prev) => prev + awards.five);
-    } else if (hits === 6) {
-      setMoney((prev) => prev + awards.six);
-    }
-  };
-  
-  // async function sendEth(send_account,send_amt) {
-  //   if(send_amt===undefined||send_amt===""){send_amt=1;}
-  //   console.log("############ 100 /lotto2/src/App.js "+send_account+":send_account");
-  //   var Web3 = require('web3');
-  //   // let web3 = new Web3(new Web3.providers.HttpProvider('https://rpc.c4ei.net/'));
-  //   await window.ethereum.enable();
-  //   // window.web3 = new Web3(window.ethereum);
-  //   let web3 = new Web3(window.ethereum);
-  //   // if(window.ethereum){ 
-  //   //   web3 = new Web3(Web3.curentProvider);
-  //   //   console.log("############ 106 Web3.curentProvider");
-  //   // }else{ 
-  //   //   web3 = new Web3(window.ethereum); 
-  //   //   console.log("############ 109 window.ethereum");
-  //   // }
-  //   // const provider = detectEthereumProvider(); //yarn add @metamask/detect-provider   //yarn remove @metamask/detect-provider
-  //   let saveData = playerNumbers[0]+" "+playerNumbers[1]+" "+playerNumbers[2]+" "+playerNumbers[3]+" "+playerNumbers[4]+" "+playerNumbers[5];
-  //   console.log("############ 114 /lotto2/src/App.js "+saveData+":saveData/"+SelectedChip+":SelectedChip/"+send_amt+":send_amt/");
-  //   let params = {
-  //     // to: toAddress,
-  //     // value: web3.utils.toWei(amount + '', 'ether'),
-  //     to: '0x286A6CE75d9f623FfbA96fC2175FD5fbE2690746' //klayMain
-  //     ,from: send_account
-  //     ,value: web3.utils.toWei(send_amt + '', 'ether')
-  //     ,data: web3.utils.toHex(saveData)
-  //   };
-  //   // console.log("############ 124 /lotto2/src/App.js "+params+":params");
-  //   return Promise.all([web3.eth.estimateGas(params), web3.eth.getGasPrice()])
-  //     .then((response) => {
-  //       const estimatedGas = response[0];
-  //       const gasPrice = response[1];
-  //       console.log("############ 128 /lotto2/src/App.js "+gasPrice+":gasPrice");
-  //       params.gas = estimatedGas;
-  //       params.gasPrice = web3.utils.toBN(gasPrice).mul(web3.utils.toBN(11)).div(web3.utils.toBN(10));  // gasPrice * 1.1
-  //       params.value = web3.utils.toBN(params.value).sub(web3.utils.toBN(params.gas).mul(web3.utils.toBN(params.gasPrice)));     // value - (gas * gasPrice)
-  //       return web3.eth.accounts.signTransaction(params, process.env.REACT_APP_PK);
-  //     })
-  //     .then((signedTx) => {
-  //       // saveLottoNum(signedTx.rawTransaction);
-  //       // return web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-  //       return web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
-  //         if (!error) {
-  //           saveLottoNum(hash , send_amt);
-  //           console.log("🎉 The hash of your transaction is: ", hash, "\n Check Alchemy's Mempool to view the status of your transaction!");
-  //         } else {
-  //           console.log("❗Something went wrong while submitting your transaction:", error)
-  //         }
-  //       });
-  //       // return hash;
-  //     });
-  // }
-// function sendERC(send_account,send_amt){
-//   const tx = myContract.methods.setData(2);
-//   const gas = await tx.estimateGas({from: address});
-//   const gasPrice = await web3.eth.getGasPrice();
-//   const data = tx.encodeABI();
-//   const nonce = await web3.eth.getTransactionCount(address);
-//   const txData = {
-//     from: address,
-//     to: myContract.options.address,
-//     data: data,
-//     gas,
-//     gasPrice,
-//     nonce, 
-//     chain: 'rinkeby', 
-//     hardfork: 'istanbul'
-//   };
-
-//   console.log(`Old data value: ${await myContract.methods.data().call()}`);
-//   const receipt = await web3.eth.sendTransaction(txData);
-//   console.log(`Transaction hash: ${receipt.transactionHash}`);
-//   console.log(`New data value: ${await myContract.methods.data().call()}`);
-// }
 
   async function sendEthByMeta(send_account,send_amt) {
+    // console.log("############ 138 /lotto2/src/App.js "+Header.jsDt.data[0].yyyywkr+"############");
     if(send_amt===undefined||send_amt===""){send_amt=1;}
     let saveData = playerNumbers[0]+" "+playerNumbers[1]+" "+playerNumbers[2]+" "+playerNumbers[3]+" "+playerNumbers[4]+" "+playerNumbers[5];
     console.log("############ 152 /lotto2/src/App.js "+saveData+":saveData/"+send_account+":send_account/"+send_amt+":send_amt/");
@@ -232,6 +102,47 @@ const App = () => {
           console.log(error)
       });
   }
+
+  const addPlayerNumbers = (number, event) => {
+    if (playerNumbers.length < 6 && !playerNumbers.includes(number)) {
+      const numbers = [...playerNumbers];
+      numbers.push(number);
+      setPlayerNumbers(numbers);
+      event.target.classList.toggle("selected");
+    }
+    if (playerNumbers.includes(number)) {
+      let numbers = [...playerNumbers];
+      numbers = numbers.filter((num) => num !== number);
+      setPlayerNumbers(numbers);
+      event.target.classList.toggle("selected");
+    }
+  };
+
+  const checkWin = (playerNumbers, drawedNumbers) => {
+    const winNumbers = [];
+
+    playerNumbers.forEach(function (number) {
+      for (let i = 0; i < 6; i++) {
+        if (number === drawedNumbers[i]) {
+          winNumbers.push(number);
+        }
+      }
+    });
+
+    const hits = winNumbers.length;
+    setHits(hits);
+
+    if (hits === 3) {
+      setMoney((prev) => prev + awards.three);
+    } else if (hits === 4) {
+      setMoney((prev) => prev + awards.four);
+    } else if (hits === 5) {
+      setMoney((prev) => prev + awards.five);
+    } else if (hits === 6) {
+      setMoney((prev) => prev + awards.six);
+    }
+  };
+
   //yarn add react-alert
   const startDraw = () => {
     if(account===undefined){
@@ -274,7 +185,6 @@ const handleConnect = () => {
   });
 }
 ///////////////////// metamask e ////////////////
-
 
 /* eslint-disable */
   return (
