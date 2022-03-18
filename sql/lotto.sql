0e82aa919af2899c2a24633b5829aa891517f206
ggbbest <www.ggb.best@gmail.com>
2022-03-19 03:41:59 +0900

    ing

--- sql/lotto.sql
+++ sql/lotto.sql
@@ -9,30 +9,6 @@ SELECT WEEK(NOW());
 SELECT WEEK('20210306');
 SELECT DATE_FORMAT(NOW(), '%Y%m%d') AS today_date FROM DUAL;
 
-
-CREATE TABLE `lotto` (
-	`idx` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`yyyy` VARCHAR(4) NOT NULL COLLATE 'utf8_unicode_ci',
-	`wk` VARCHAR(2) NOT NULL COLLATE 'utf8_unicode_ci',
-	`yyyymmdd` VARCHAR(8) NOT NULL COLLATE 'utf8_unicode_ci',
-	`chips` INT(4) NOT NULL,
-	`numb_tot` VARCHAR(20) NULL COLLATE 'utf8_unicode_ci',
-	`numb1` TINYINT(2) NOT NULL,
-	`numb2` TINYINT(2) NOT NULL,
-	`numb3` TINYINT(2) NOT NULL,
-	`numb4` TINYINT(2) NOT NULL,
-	`numb5` TINYINT(2) NOT NULL,
-	`numb6` TINYINT(2) NOT NULL,
-	`addr` VARCHAR(70) NOT NULL COLLATE 'utf8_unicode_ci',
-	`sendTr` VARCHAR(100) NULL COLLATE 'utf8_unicode_ci',
-	`win_rank` INT(1) NOT NULL DEFAULT '0',
-	`regdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`idx`) USING BTREE
-)
-COLLATE='utf8_unicode_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
 --////////////////////////////////////////////////////////////////////////////////////////////////
 CREATE TABLE `lotto` (
 	`idx` BIGINT(20) NOT NULL AUTO_INCREMENT,
@@ -61,6 +37,7 @@ CREATE TABLE `lotto` (
 	`c_tot` DECIMAL(2,1) NOT NULL DEFAULT '0.0',
 	`c_rank` TINYINT(1) NOT NULL DEFAULT '0',
 	`sendYN` VARCHAR(1) NOT NULL DEFAULT 'N' COLLATE 'utf8_unicode_ci',
+	`sendchips` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
 	`sendDate` DATETIME NULL,
 	`memo` VARCHAR(100) NULL COLLATE 'utf8_unicode_ci',
 	PRIMARY KEY (`idx`) USING BTREE,
@@ -69,7 +46,7 @@ CREATE TABLE `lotto` (
 )
 COLLATE='utf8_unicode_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=168
+AUTO_INCREMENT=231
 ;
 
 INSERT INTO lotto_num (`yyyy`,`wk`,`yyyymmdd`) SELECT '2202','12','22020326' FROM DUAL;
@@ -93,4 +70,43 @@ select YEAR(NOW()), WEEK(NOW()), DATE_FORMAT(NOW(), '%Y%m%d'),
 ,'1,2,3,4,5,6',1,2,3,4,5,6
 from dual;
 
+CREATE TABLE `lotto_sum_money` (
+	`idx` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`yyyy` VARCHAR(4) NOT NULL COLLATE 'utf8_unicode_ci',
+	`wk` VARCHAR(2) NOT NULL COLLATE 'utf8_unicode_ci',
+	`coin_name` VARCHAR(10) NULL COLLATE 'utf8_unicode_ci',
+	`sumchips` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
+	`sum_sendchips` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
+	`real_tot` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
+	`real_half_tot` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
+	`amt1st` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
+	`amt2nd` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
+	`amt3rd` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
+	`regdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`idx`) USING BTREE,
+	INDEX `yyyy_wk_coin_name` (`yyyy`, `wk`, `coin_name`) USING BTREE
+)
+COMMENT='회차별 로또 금액 합 real_half_tot 금액을 1,2,3 등이 나눔'
+COLLATE='utf8_unicode_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=11
+;
 
+CREATE TABLE `lotto_result` (
+	`idx` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`yyyy` VARCHAR(4) NOT NULL COLLATE 'utf8_unicode_ci',
+	`wk` VARCHAR(2) NOT NULL COLLATE 'utf8_unicode_ci',
+	`chips_tot` DECIMAL(10,4) NOT NULL DEFAULT '0.0000',
+	`coin_name` VARCHAR(10) NULL COLLATE 'utf8_unicode_ci',
+	`r1` INT(4) NOT NULL DEFAULT '0',
+	`r2` INT(4) NOT NULL DEFAULT '0',
+	`r3` INT(4) NOT NULL DEFAULT '0',
+	`r4` INT(4) NOT NULL DEFAULT '0',
+	`r5` INT(4) NOT NULL DEFAULT '0',
+	`regdate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`idx`) USING BTREE
+)
+COLLATE='utf8_unicode_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=4
+;
--- sql/lotto_rank.sql
+++ sql/lotto_rank.sql
@@ -1,7 +1,7 @@
 -- make test data 
 INSERT INTO lotto (	`yyyy` ,`wk` ,`yyyymmdd` ,`chips` ,`numb_tot` ,`numb1` ,
 `numb2` ,`numb3` ,`numb4` ,`numb5` ,`numb6` ,`chainId` ,`coin_name` ,`addr` ,`sendTr` )
-SELECT `yyyy` ,'10' `wk` ,`yyyymmdd` ,`chips` ,`numb_tot` ,`numb1` ,`numb2` ,`numb3` ,
+SELECT `yyyy` ,'1' `wk` ,`yyyymmdd` ,`chips` ,`numb_tot` ,`numb1` ,`numb2` ,`numb3` ,
 `numb4` ,`numb5` ,`numb6` ,`chainId` ,`coin_name` ,`addr` ,`sendTr`
 FROM lotto WHERE `yyyy`='2022' AND `wk`='11';
 -- DELETE FROM lotto WHERE `yyyy`='2022' AND `wk`='10';
@@ -17,7 +17,9 @@ AND `yyyy`='2022' AND `wk`='10';
 -- match logic lotto.c4ei.net
 ------------------------------------------------------------------------------
 -- DECLEAR
-set @yyyy='2022', @wk='10', @winNumb = '18,25,34,35,39,45', @w1=18,@w2=25,@w3=34,@w4=35,@w5=39,@w6=45, @winPlusNumb=43; 
+-- 6,11,15,24,32,39  28 --996 2022-01-01
+set @yyyy='2022', @wk='1', @winNumb = '6,11,15,24,32,39', @w1=6,@w2=11,@w3=15,@w4=24,@w5=32,@w6=39, @winPlusNumb=28; 
+
 -- step 1
 UPDATE lotto SET 
 	c1 = case when numb1 IN (@w1,@w2,@w3,@w4,@w5,@w6) then 1 ELSE 0 END, 
@@ -32,16 +34,34 @@ commit;
 -- step 2
 UPDATE lotto SET c_tot = (c1+c2+c3+c4+c5+c6) WHERE yyyy=@yyyy AND wk=@wk;
 commit;
-
 -- step 3 - find 5 numb match and check plus num set tot 5.5 --> 2nd win
 UPDATE lotto SET c_tot = 5.5 WHERE yyyy=@yyyy AND wk=@wk AND c_tot = 5 
     AND (numb1=@winPlusNumb OR numb2=@winPlusNumb OR numb3=@winPlusNumb OR numb4=@winPlusNumb OR numb5=@winPlusNumb OR numb6=@winPlusNumb);
 commit;
-
 -- step 4 - set rank
-UPDATE lotto SET c_rank =  case c_tot when 6 then 1 when 5.5 then 2 when 5 then 3 when 4 then 4 when 3 then 5 ELSE 0 END 
+UPDATE lotto 
+SET c_rank =  case c_tot when 6 then 1 when 5.5 then 2 when 5 then 3 when 4 then 4 when 3 then 5 ELSE 0 END 
 WHERE yyyy=@yyyy AND wk=@wk AND c_tot > 2;
 commit;
+
+-- step 5 -- 4,5 sending ship calcu
+UPDATE lotto 
+SET sendchips = case c_rank when 4 then chips*50 when 5 then chips*5 ELSE 0 END 
+WHERE yyyy=@yyyy AND wk=@wk AND (c_rank =4 OR c_rank =5) ;
+COMMIT;
+
+-- step 6 -- 4,5 sending ship calcu
+DELETE FROM lotto_sum_money WHERE yyyy=@yyyy AND wk=@wk;COMMIT;
+INSERT INTO lotto_sum_money (yyyy,wk, coin_name,sumchips,sum_sendchips,real_tot,real_half_tot,amt1st,amt2nd,amt3rd)
+SELECT yyyy,wk, coin_name, SUM(chips) sumchips, SUM(sendchips) sum_sendchips, (SUM(chips)-SUM(sendchips)) real_tot
+, (SUM(chips)-SUM(sendchips))*0.5 real_half_tot
+, (SUM(chips)-SUM(sendchips))*0.5*0.75 amt1st
+, (SUM(chips)-SUM(sendchips))*0.5*0.125 amt2nd
+, (SUM(chips)-SUM(sendchips))*0.5*0.125 amt3rd
+FROM lotto 
+WHERE `yyyy`=@yyyy AND `wk`=@wk 
+GROUP BY yyyy,wk, coin_name;
+COMMIT;
 
 
 SELECT * FROM lotto  WHERE `yyyy`='2022' AND `wk`='10' AND c_rank >0;
